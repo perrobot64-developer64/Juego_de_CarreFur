babe68f62284e615e4b09348a0d113c64c2aa49b
perrobot64-developer64 <138079625+perrobot64-developer64@users.noreply.github.com>
2025-09-13 05:09:46 -0300

    desing del menu de pausa

--- Escenas/menu de pausa/PauseMenu.gd
+++ Escenas/menu de pausa/PauseMenu.gd
@@ -2,18 +2,17 @@ extends CanvasLayer
 
 @onready var panel: Control = $Control
 @onready var fondo: ColorRect = $ColorRect
-@onready var bg: ColorRect = $algo
-@onready var icono: Sprite2D = $algo/pausa
+@onready var icono: Sprite2D = $Control/pausa
+@onready var BG: Sprite2D = $ParallaxBackground/ParallaxLayer/test
+
 var tween: Tween
 
 func _ready():
 	panel.visible = false
 	fondo.visible = true
 	fondo.color.a = 0.0
-	bg.visible = true
-	bg.color.a = 0.0
 	icono.visible = false
-	
+	BG.modulate.a = 0.0
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"): 
@@ -24,16 +23,18 @@ func toggle_pause():
 		# Quitar pausa
 		get_tree().paused = false
 		panel.visible = false
-		bg.visible = false
+		#bg.visible = false
 		icono.visible = false
+		BG.visible = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		fade_to(0.0)
 	else:
 		# Activar pausa
 		get_tree().paused = true
 		panel.visible = true
-		bg.visible = true
+		#bg.visible = true
 		icono.visible = true
+		BG.visible = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		fade_to(0.5)
 
@@ -42,8 +43,10 @@ func fade_to(alpha: float):
 		tween.kill()
 	tween = create_tween()
 	tween.set_process_mode(Tween.TweenProcessMode.TWEEN_PROCESS_IDLE)
-	tween.tween_property(fondo, "color:a", alpha, 0.3)
-	tween.tween_property(bg, "color:a", alpha, 0.6)
+	
+	var dur := 0.4 # ejemplo, todos con la misma velocidad
+	tween.tween_property(fondo, "color:a", alpha, dur)
+	tween.tween_property(BG, "modulate:a", alpha, dur)
 
 # Función conectada al botón "Continuar"
 func _on_BotonContinuar_pressed():
